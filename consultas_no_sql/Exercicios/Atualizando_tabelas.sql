277391eba529691ff606978fc9e7169038835123
Vitor Oliveira <vitorsk807@gmail.com>
2023-01-23 18:35:36 -0300

    Atualizando tabelas com UPDATE SET

---
+++ consultas_no_sql/Exercicios/Atualizando_tabelas.sql
@@ -0,0 +1,10 @@
+USE skaila;
+
+UPDATE payment --Esse comando serve para indicar a tabela que sera atualizada
+SET 
+    amount = 15.99
+WHERE payment_id = 1 
+
+-- UPDATE esta pedindo para atualizar a tabela payment 
+-- SET seleciona a coluna amount 
+-- WHERE seleciona a linha = 1 
