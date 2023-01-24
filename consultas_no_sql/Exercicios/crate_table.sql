438d9eacd2f0fd7384f4cdb1816031c40e48a91c
Vitor Oliveira <vitorsk807@gmail.com>
2023-01-24 15:45:52 -0300

    Atualizando comentario

--- consultas_no_sql/Exercicios/crate_table.sql
+++ consultas_no_sql/Exercicios/crate_table.sql
@@ -1,4 +1,4 @@
 USE sakila;
 
 CREATE TABLE payment_backup AS -- Esse operador serve para copiar uma tabela 
-SELECT * FROM payment
+SELECT * FROM payment -- Seleciona toda a tabela payment para ser copiada e fazer o backup 
