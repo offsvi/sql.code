35139b8366c7e8ad95c7a5095acf45a2fbb016f5
Vitor Oliveira <vitorsk807@gmail.com>
2023-01-24 15:47:18 -0300

    Atualizando comentários para explicar melhor o codigo

--- consultas_no_sql/Exercicios/operador_isnull.sql
+++ consultas_no_sql/Exercicios/operador_isnull.sql
@@ -2,4 +2,4 @@
 -- Esse operador solicita que busque os campos que não estão nulos 
 USE sakila;
 SELECT * FROM  address 
-WHERE address2 IS NULL 
+WHERE address2 IS NULL  -- Esse operador filtra toda a coluna address2 que está vazia 
