8c95eb1e89a9d66c33c9bc98926c7f18f07288b0
Vitor Oliveira <vitorsk807@gmail.com>
2023-01-24 15:45:05 -0300

    Inserindo commentarios

--- consultas_no_sql/Exercicios/consulta_de_banco.sql
+++ consultas_no_sql/Exercicios/consulta_de_banco.sql
@@ -1,4 +1,4 @@
 -- Fazendo uma consulta no banco de dados sakila
 USE sakila 
-SELECT * FROM payment 
-WHERE first_name and last_name = 10 
+SELECT * FROM payment -- Seleciona toda a tabela payment
+WHERE first_name and last_name = 10 -- Filtra first_name e os last_name que for = 10
